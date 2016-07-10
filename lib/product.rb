a2ee055ce779845fffa4d0cb29389512fbe61398
QilinGu <gump423@gmail.com>
2016-07-10 11:48:51 -0700

    Add ability of finding items and tracking price and stock

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,12 @@
 class Product
-	attr_reader :title
+	attr_reader :title, :price, :stock
 
 	@@products = []
 	
 	def initialize(options={})
 		@title = options[:title]
+		@price = options[:price]
+		@stock = options[:stock]
 		add_to_products
 	end
 
@@ -12,10 +14,14 @@ class Product
 		@@products
 	end
 
+	def self.find_by_title(title)
+		@@products.find {|product| product.title == title}
+	end
+
 	private
 
 	def add_to_products
-		product = @@products.find {|product| product.title == @title}
+		product = self.class.find_by_title(@title)
 		if product
 			raise DuplicateProductError, "#{@title} already exists."
 		else
